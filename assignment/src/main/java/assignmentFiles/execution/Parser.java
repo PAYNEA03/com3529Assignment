57ac7f6b7cab660981072249fd516432e3fdbf1b
PAYNEA03 <46678732+PAYNEA03@users.noreply.github.com>
2021-04-10 10:37:32 +0100

    added else parser
    now records location of else statements. will help for instrumentation

--- assignment/src/main/java/assignmentFiles/execution/Parser.java
+++ assignment/src/main/java/assignmentFiles/execution/Parser.java
@@ -3,11 +3,10 @@ package assignmentFiles.execution;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 
-import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.stmt.IfStmt;
+import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.metamodel.MethodDeclarationMetaModel;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -17,8 +16,8 @@ import java.util.List;
 public class Parser {
 
 //    private static final String FILE_PATH = "src/main/java/assignmentFiles/subjectFiles/Triangle.java";
-//    private static final String FILE_PATH = "src/main/java/assignmentFiles/subjectFiles/BMICalculator.java";
-    private static final String FILE_PATH = "src/main/java/assignmentFiles/subjectFiles/Calendar.java";
+    private static final String FILE_PATH = "src/main/java/assignmentFiles/subjectFiles/BMICalculator.java";
+//    private static final String FILE_PATH = "src/main/java/assignmentFiles/subjectFiles/Calendar.java";
 //    private static final String FILE_PATH = "src/main/java/assignmentFiles/subjectFiles/VendingMachine.java";
 
 
@@ -36,7 +35,7 @@ public class Parser {
 
 
     /**
-     * MethodNameCollector
+     * ifStmtCollector
      * passes in a list object and adding criteria to the list and returning the obejct
      * */
     private static class IfStmtCollector extends VoidVisitorAdapter<List<HashMap>> {
@@ -45,20 +44,48 @@ public class Parser {
             super.visit(md, collector);
             
             collector.add(parseIf(md));
+
+            if (md.hasElseBlock()){
+                collector.add(parseElse(md.getElseStmt().get()));
+            }
         }
 
         public static HashMap<String, Object> parseIf(IfStmt n) {
             HashMap<String, Object> ifStmt = new HashMap<>();
-//            MethodDeclaration m = (MethodDeclaration) n.getParentNode().get().getParentNode().get();
 
-//            ifStmt.put("methodName", m.getNameAsString());
+/*
+            @TODO
+              MethodDeclaration m = (MethodDeclaration) n.getParentNode().get().getParentNode().get();
+              ifStmt.put("methodName", m.getNameAsString());
+              above comments retrieve name, however throw error on calendar class so commented out until resolved
+*/
+
+            ifStmt.put("type", "IF");
             ifStmt.put("lineStart", n.getBegin().get().line);
             ifStmt.put("lineEnd", n.getEnd().get().line);
             ifStmt.put("conditions", n.getCondition());
-//            n.getCondition().asBinaryExpr() - can get variable names and also predicate operator,
-//            commented out as throws error on Calendar.java
+
+/*
+          @TODO
+            ifStmt.put("object", n); stores the full object for later access. Not in use whilst debugging
+            n.getCondition().asBinaryExpr() - can get variable names and also predicate operator,
+            commented out as throws error on Calendar.java
+*/
 
             return ifStmt;
+        }
+
+        public static HashMap<String, Object> parseElse(Statement n) {
+            HashMap<String, Object> elseStmt = new HashMap<>();
+
+            elseStmt.put("type", "ELSE");
+            elseStmt.put("lineStart", n.getBegin().get().line);
+            elseStmt.put("lineEnd", n.getEnd().get().line);
+
+            /* @TODO elseStmt.put("object", n); stores the full object for later access. Not in use whilst debugging
+            */
+
+            return elseStmt;
         }
 
     }
---
+++ assignment/src/main/java/assignmentFiles/subjectFiles/SignUtils.java
@@ -0,0 +1,26 @@
+package assignmentFiles.subjectFiles;
+
+public class SignUtils {
+
+    public static boolean isNegative(int n) {
+        return n < 0;
+    }
+
+    public static boolean isPositive(int n) {
+        return n > 0;
+    }
+
+    public static int sign(int n) {
+        int s = 0;
+
+        if (n > 0) {
+            s = 1;
+        }
+
+        if (n < 0) {
+            s = -1;
+        }
+
+        return s;
+    }
+}
