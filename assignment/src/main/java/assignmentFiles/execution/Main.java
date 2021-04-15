d4ff8f49e1dcd943d860f42b009f39d6d876dbca
PAYNEA03 <46678732+PAYNEA03@users.noreply.github.com>
2021-04-15 08:39:15 +0100

    Update Main.java
    Updated TODO, cleared out unused methods from main.java

--- assignment/src/main/java/assignmentFiles/execution/Main.java
+++ assignment/src/main/java/assignmentFiles/execution/Main.java
@@ -26,11 +26,6 @@ public class Main {
 
         CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH[2]));
 
-//        @todo 4.1: analyse methods, obtain predicates and conditions.
-
-//        @todo 4.1.2/4.3: using obtained methods and predicates, create an instrumented file with
-//          logging statements
-
         Instrument classMethods = Instrument.parseClass(cu);
 
 //        @todo 4.2: generate test requirements (branch coverage/MCDC)
@@ -44,58 +39,5 @@ public class Main {
 
 
     }
-
-    private static void parseAllFiles() throws FileNotFoundException {
-        for (String file: FILE_PATH) {
-            System.out.println("");
-            System.out.println("File to be parsed: " + file);
-
-            // Parse File using Java Parser
-            CompilationUnit cu = StaticJavaParser.parse(new File(file));
-
-
-//            /**
-//             * Javaparser visitor for if and else statements, returns a hasmap with details of the conditions
-//             * */
-//            List<HashMap> ifStmt = new ArrayList<>();
-//            VoidVisitor<List<HashMap>> ifStmtCollector = new Parser.IfStmtCollector();
-//
-//            ifStmtCollector.visit(cu, ifStmt);
-//            ifStmt.forEach(n ->
-//                    System.out.println(n));
-
-//            for (HashMap h:ifStmt) {
-//                IfStmt i = (IfStmt) h.get("object");
-//
-//                System.out.println(i);
-//            }
-
-            /**
-             * Javaparser visitor for method declarations, returns a hashmap with method names and their params
-             * */
-
-            List<HashMap> methodNames = new ArrayList<>();
-            VoidVisitor<List<HashMap>> methodCollector = new Parser.MethodCollector();
-
-            methodCollector.visit(cu, methodNames);
-            methodNames.forEach(n ->
-                    System.out.println(n));
-
-            System.out.println(cu);
-        }
-    }
-
-    private static void writeFile(String file, String fileName) throws IOException {
-//        String[] file = WriteToFile.fileInstrument(filePath);
-
-//        String instrumentedFile = WriteToFile.writeInstrumentedFile(file, fileName);
-
-        // checks to see if created file exists (https://alvinalexander.com/java/java-file-exists-directory-exists/)
-//        File tmpDir = new File(instrumentedFile);
-//        boolean exists = tmpDir.exists();
-//
-//        System.out.println("has file been created? " + exists + " @ " + instrumentedFile);
-    }
-
 
 }
