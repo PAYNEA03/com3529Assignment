9781b8604562871ca8ea63a28f13ceed9e86e542
Ambrose Robinson <56968383+ambroser53@users.noreply.github.com>
2021-04-15 15:50:00 +0100

    Revert "my stupid push"
    This reverts commit eb9ee62f07e22d9779fd44967207b6892960b58f.

--- assignment/src/main/java/assignmentFiles/execution/TestDataGenerator.java
+++ assignment/src/main/java/assignmentFiles/execution/TestDataGenerator.java
@@ -2,16 +2,7 @@ package assignmentFiles.execution;
 
 //import assignmentFiles.instrumentedFiles.*;
 import assignmentFiles.subjectFiles.Triangle;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.type.Type;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.*;
-import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
@@ -22,62 +13,25 @@ public class TestDataGenerator {
     static final int MIN_INT = -10;
     static final int MAX_INT = 10;
 
-    HashMap<String, HashMap<String,Type>> methodList;
+    public static void randomBranchGeneration() {
 
-    //the constructor is called and set up the parameters for the coverage
-    // (i.e. used when checking if an input was a new successful test to be added to the test suite)
-    public TestDataGenerator(String coverageCriteria, String generationMethod, HashMap<String, HashMap<String, Type>> methodList){
-        this.methodList = methodList;
-        if (coverageCriteria.equals("condition")){
-
-        }
-        else { // for now lets just assume that otherwise it was (coverageCriteria.equals("correlatedMCDC"))
-
-        }
-        if (generationMethod == "random"){
-
-        }
-        else { //for now lets just assume if not random then its (generationMethod == "search")
-
-        }
-    }
-
-    public void testCaseGeneration(CompilationUnit cu) {
-
-
-        //get an instance of the class
-        Class clazz = cu.getClass();
-        Method [] allMethods = clazz.getMethods();
-
-
-        //iterate through each method
-        for (HashMap.Entry<String,HashMap<String,Type>> mapElement : methodList.entrySet()) {
-            String methodName = mapElement.getKey();
-            HashMap<String,Type> parameterList = mapElement.getValue();
-
-
-        }   //TODO come back to or delete
-
-        for (Method method:allMethods){
-            
-        }
-
-
-        //set up how the inputs are generated
         Random r = new Random();
         Set<Integer> coveredBranches = new TreeSet<>();
 
-        //invoke the method once to get the type of the output and record it
-
         for (int i=0; i < ITERATIONS; i ++) {
-            //generate inputs
 
-            //get result from said inputs
+//            @todo get methods to test
+//              * create variables for that methods parameters
+//              * call method with variables
+//            int side1 = randomInt(r);
+//            int side2 = randomInt(r);
+//            int side3 = randomInt(r);
+//            System.out.println((i+1) + ": [" + side1 + ", " + side2 + ", " + side3 + "]");
+//            Triangle.Type result = instrumentedClassify(side1, side2, side3, coveredBranches);
+//            System.out.println("-> " + result);
+            Object var = 2;
 
-            //is this input wanted?
-            //here check all the coverage business relative to what was set up
-
-            //check if full/enough coverage has been reached if so break
+//            InstrumentedCalendar.daysBetweenTwoDates(var,var,var,var,var,var,var);
         }
 
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -85,36 +39,11 @@ public class TestDataGenerator {
         System.out.println("Covered Branch IDs: " + coveredBranches);
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-        //output the inputs for each test-case in form HashMap<Method Name,HashMap<parameter input, input type>>
 
-        //next
-        //https://stackoverflow.com/questions/160970/how-do-i-invoke-a-java-method-when-given-the-method-name-as-a-string
-        //https://docs.oracle.com/javase/tutorial/java/generics/types.html
-        //https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwid3N7GjP7vAhViWhUIHRvpDBwQFjAAegQIBhAD&url=http%3A%2F%2Fcrest.cs.ucl.ac.uk%2Ffileadmin%2Fcrest%2Fsebasepaper%2FGhaniC09b.pdf&usg=AOvVaw1kA1DwCCIHOMPkwjVTtfXV
-
-    }
-
-    //this uses the url to get the class instance although not sure if it works
-    private Class getClassInstance(String className) throws MalformedURLException, ClassNotFoundException{
-        String classPath = System.getProperty("user.dir")+"src/main/java/assignmentFiles/instrumentedFiles/"+className+".java";
-        File f = new File(classPath);
-        URL[] cp = {f.toURI().toURL()};
-        URLClassLoader urlcl = new URLClassLoader(cp);
-        Class clazz = urlcl.loadClass("instrumentedFiles.Instrumented"+className);
-        return clazz;
     }
 
     public static void searchBasedGeneration() {
 
-        //return method, each parameter - n test cases
-        //HashMap<String,HashMap<String,String>>
-        // or
-        //HashMap<String,HashMap<Class<?>,String>>
-
-        //TODO look into Class<?>
-
-
-        //no coveredBranches in method means that theres no branches so trivially can only make one test case
     }
 
     /** randomInt is taken from the week5 lectures of the RandomlyTestTriangle class and
